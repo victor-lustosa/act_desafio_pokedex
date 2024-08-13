69b2563de4d7b714ef005a2671a4866701e9939e
Victor Lustosa <victor.olustosa@outlook.com>
2024-08-13 00:27:08 -0300

    refactor: fechando teclado se tocar no app

--- lib/app/ui/views/home_view.dart
+++ lib/app/ui/views/home_view.dart
@@ -44,135 +44,140 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBarWidget(
-          store: store,
-        ),
-        body: SizedBox(
-          width: context.sizeOf.width,
-          height: context.sizeOf.height,
-          child: Observer(
-            builder: (_) {
-              if (store.state is DataFetchedState) {
-                pokemonsList = (store.state as DataFetchedState).entities;
-                if (pokemonsList.isEmpty) {
-                  return defaultMessage(
-                      'Não foi encontrado nenhum pockemon com esse nome.',
-                      Colors.cyan);
-                }
-                return Column(
-                  children: [
-                    Expanded(
-                      child: ListView.separated(
-                        separatorBuilder: (BuildContext context, int index) {
-                          return const SizedBox(height: 16);
-                        },
-                        padding: const EdgeInsets.only(
-                            left: 15, right: 15, bottom: 20),
-                        scrollDirection: Axis.vertical,
-                        shrinkWrap: true,
-                        controller: store.scrollController,
-                        physics: const AlwaysScrollableScrollPhysics(),
-                        itemCount: pokemonsList.length,
-                        itemBuilder: (context, index) {
-                          return InkWell(
-                            onTap: () => store.showDialog(pokemonsList[index]),
-                            child: Card(
-                              elevation: 2,
-                              color: Colors.white,
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    margin: const EdgeInsets.only(
-                                        left: 16, top: 10),
-                                    child: Text(
-                                      pokemonsList[index].name,
-                                      style: AppFonts.defaultFont(
-                                        color: Colors.black87,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    ),
-                                  ),
-                                  Container(
-                                    height: 200,
-                                    width: 200,
-                                    decoration: BoxDecoration(
-                                      borderRadius: const BorderRadius.all(
-                                        Radius.circular(18),
-                                      ),
-                                      image: DecorationImage(
-                                        fit: BoxFit.fill,
-                                        image: NetworkImage(
-                                          pokemonsList[index].image,
+      child: GestureDetector(
+        onTap: (){
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        child: Scaffold(
+          appBar: AppBarWidget(
+            store: store,
+          ),
+          body: SizedBox(
+            width: context.sizeOf.width,
+            height: context.sizeOf.height,
+            child: Observer(
+              builder: (_) {
+                if (store.state is DataFetchedState) {
+                  pokemonsList = (store.state as DataFetchedState).entities;
+                  if (pokemonsList.isEmpty) {
+                    return defaultMessage(
+                        'Não foi encontrado nenhum pockemon com esse nome.',
+                        Colors.cyan);
+                  }
+                  return Column(
+                    children: [
+                      Expanded(
+                        child: ListView.separated(
+                          separatorBuilder: (BuildContext context, int index) {
+                            return const SizedBox(height: 16);
+                          },
+                          padding: const EdgeInsets.only(
+                              left: 15, right: 15, bottom: 20),
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          controller: store.scrollController,
+                          physics: const AlwaysScrollableScrollPhysics(),
+                          itemCount: pokemonsList.length,
+                          itemBuilder: (context, index) {
+                            return InkWell(
+                              onTap: () => store.showDialog(pokemonsList[index]),
+                              child: Card(
+                                elevation: 2,
+                                color: Colors.white,
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      margin: const EdgeInsets.only(
+                                          left: 16, top: 10),
+                                      child: Text(
+                                        pokemonsList[index].name,
+                                        style: AppFonts.defaultFont(
+                                          color: Colors.black87,
+                                          fontWeight: FontWeight.w500,
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
+                                    Container(
+                                      height: 200,
+                                      width: 200,
+                                      decoration: BoxDecoration(
+                                        borderRadius: const BorderRadius.all(
+                                          Radius.circular(18),
+                                        ),
+                                        image: DecorationImage(
+                                          fit: BoxFit.fill,
+                                          image: NetworkImage(
+                                            pokemonsList[index].image,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                          );
-                        },
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  );
+                } else if (store.state is ExceptionState) {
+                  return defaultMessage(
+                      (store.state as ExceptionState).message, Colors.red);
+                } else {
+                  return const Center(
+                    child: LoadingWidget(
+                      androidRadius: 4,
+                      iosRadius: 14,
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          resizeToAvoidBottomInset: false,
+          floatingActionButton: Observer(
+            builder: (context) {
+              return Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AnimatedSlide(
+                      duration: const Duration(microseconds: 1000),
+                      offset: store.isPreviousButtonVisible
+                          ? Offset.zero
+                          : const Offset(0, 2),
+                      child: FloatingButtonWidget(
+                        heroTag: 'previous',
+                        icon: Icons.arrow_back_rounded,
+                        backgroundColor: Colors.cyan,
+                        iconColor: Colors.white,
+                        size: 33,
+                        action: store.callPreviousPage,
+                      ),
+                    ),
+                    AnimatedSlide(
+                      duration: const Duration(microseconds: 1000),
+                      offset: store.isNextButtonVisible
+                          ? Offset.zero
+                          : const Offset(0, 2),
+                      child: FloatingButtonWidget(
+                        heroTag: 'next',
+                        icon: Icons.arrow_forward_rounded,
+                        backgroundColor: Colors.cyan,
+                        iconColor: Colors.white,
+                        size: 33,
+                        action: store.callNextPage,
                       ),
                     ),
                   ],
-                );
-              } else if (store.state is ExceptionState) {
-                return defaultMessage(
-                    (store.state as ExceptionState).message, Colors.red);
-              } else {
-                return const Center(
-                  child: LoadingWidget(
-                    androidRadius: 4,
-                    iosRadius: 14,
-                  ),
-                );
-              }
-            },
+                ),
+              );
+            }
           ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        resizeToAvoidBottomInset: false,
-        floatingActionButton: Observer(
-          builder: (context) {
-            return Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  AnimatedSlide(
-                    duration: const Duration(microseconds: 1000),
-                    offset: store.isPreviousButtonVisible
-                        ? Offset.zero
-                        : const Offset(0, 2),
-                    child: FloatingButtonWidget(
-                      heroTag: 'previous',
-                      icon: Icons.arrow_back_rounded,
-                      backgroundColor: Colors.cyan,
-                      iconColor: Colors.white,
-                      size: 33,
-                      action: store.callPreviousPage,
-                    ),
-                  ),
-                  AnimatedSlide(
-                    duration: const Duration(microseconds: 1000),
-                    offset: store.isNextButtonVisible
-                        ? Offset.zero
-                        : const Offset(0, 2),
-                    child: FloatingButtonWidget(
-                      heroTag: 'next',
-                      icon: Icons.arrow_forward_rounded,
-                      backgroundColor: Colors.cyan,
-                      iconColor: Colors.white,
-                      size: 33,
-                      action: store.callNextPage,
-                    ),
-                  ),
-                ],
-              ),
-            );
-          }
         ),
       ),
     );
